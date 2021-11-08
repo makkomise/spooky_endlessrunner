7a0a8a3a0905d40caca7ff772b71db65dbde2dbb
pekkauitto <pekka.uitto@gmail.com>
2021-11-08 18:08:35 +0200

    Intro videon lisäys
    Intro scene lisätty buildiin ja asetettu scene 0:ksi.
    Scene 1 latautuu videon jälkeen tai ESC nappulaa painamalla voi skipata videon.

---
+++ spooky_endlessrunner/Assets/Scripts/wait.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class wait : MonoBehaviour
+{
+
+    public float wait_time = 38f;
+    void Start()
+    {
+        StartCoroutine(Wait_for_intro());
+    }
+
+    IEnumerator Wait_for_intro()
+    {
+        yield return new WaitForSeconds(wait_time);
+
+        SceneManager.LoadScene(1);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
